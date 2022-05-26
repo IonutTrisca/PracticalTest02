76c79d6c6fd0cebd538bba3dd044d39580930df2
IonutTrisca <trisca46@gmail.com>
2022-05-26 08:28:20 +0300

    Added project
    Signed-off-by: IonutTrisca <trisca46@gmail.com>

---
+++ PracticalTest02/app/src/main/java/ro/pub/cs/systems/eim/practicaltest02/view/PracticalTest02MainActivity.java
@@ -0,0 +1,15 @@
+package ro.pub.cs.systems.eim.practicaltest02.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ro.pub.cs.systems.eim.practicaltest02.R;
+
+public class PracticalTest02MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_practical_test02_main);
+    }
+}
